19003a16644784c843a459927ff1b21ec00580df
백호동 <whitetiger@hashbrand.kr>
2021-06-06 14:21:25 +0900

    user 추가

--- simple-todos-react/imports/ui/App.jsx
+++ simple-todos-react/imports/ui/App.jsx
@@ -1,68 +1,100 @@
-import React, {useState} from 'react';
+import React, { useState, Fragment } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
 import { TaskForm } from './TaskForm';
+import { LoginForm } from './LoginForm';
+
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked,
+    },
+  });
+};
+
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
 
 export const App = () => {
+  const user = useTracker(() => Meteor.user());
+
   const [hideCompleted, setHideCompleted] = useState(false);
-  //필터 (체크되지 않은것만 가져오기)
+
   const hideCompletedFilter = { isChecked: { $ne: true } };
 
-  const tasks = useTracker(() =>
-    TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
-      sort: { createdAt: -1 },
-    }).fetch()
-  );
+  const userFilter = user ? { userId: user._id } : {};
 
-  const pendingTasksCount = useTracker(()=> 
-    TasksCollection.find(hideCompletedFilter).count()
-  )
+  const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter };
 
-  const pendingTasksTitle =`${
+  const tasks = useTracker(() => {
+    if (!user) {
+      return [];
+    }
+
+    return TasksCollection.find(
+      hideCompleted ? pendingOnlyFilter : userFilter,
+      {
+        sort: { createdAt: -1 },
+      }
+    ).fetch();
+  });
+
+  const pendingTasksCount = useTracker(() => {
+    if (!user) {
+      return 0;
+    }
+
+    return TasksCollection.find(pendingOnlyFilter).count();
+  });
+
+  const pendingTasksTitle = `${
     pendingTasksCount ? ` (${pendingTasksCount})` : ''
   }`;
 
-  const toggleChecked = ({_id, isChecked}) => {
-    TasksCollection.update(_id, {
-      $set: {
-        isChecked: !isChecked
-      }
-    })
-  }
-
-  const deleteTask = ({_id}) => TasksCollection.remove(_id);
+  const logout = () => Meteor.logout();
 
   return (
     <div className="app">
       <header>
         <div className="app-bar">
           <div className="app-header">
-          <h1>
-            📝️ To Do List
-            {pendingTasksTitle}
-          </h1>
+            <h1>
+              📝️ To Do List
+              {pendingTasksTitle}
+            </h1>
           </div>
         </div>
       </header>
 
       <div className="main">
-        <TaskForm />
-        <div className="filter">
-         <button onClick={() => setHideCompleted(!hideCompleted)}>
-           {hideCompleted ? 'Show All' : 'Hide Completed'}
-         </button>
-        </div>
-        <ul className="tasks">
-          {tasks.map(task => (
-            <Task
-              key={task._id}
-              task={task}
-              onCheckboxClick={toggleChecked}
-              onDeleteClick={deleteTask}
-            />
-          ))}
-        </ul>
+        {user ? (
+          <Fragment>
+            <div className="user" onClick={logout}>
+              {user.username} 🚪
+            </div>
+
+            <TaskForm user={user} />
+
+            <div className="filter">
+              <button onClick={() => setHideCompleted(!hideCompleted)}>
+                {hideCompleted ? 'Show All' : 'Hide Completed'}
+              </button>
+            </div>
+
+            <ul className="tasks">
+              {tasks.map(task => (
+                <Task
+                  key={task._id}
+                  task={task}
+                  onCheckboxClick={toggleChecked}
+                  onDeleteClick={deleteTask}
+                />
+              ))}
+            </ul>
+          </Fragment>
+        ) : (
+          <LoginForm />
+        )}
       </div>
     </div>
   );
--- simple-todos-react/readme.md
+++ simple-todos-react/readme.md
@@ -23,4 +23,13 @@ export const App = () => {
   );
 }; 
 ```
-test
+
+## 비밀번호 인증
+
+> Meteor에는 이미 기본 인증 및 계정 관리 시스템이 기본 제공되므로  <br/> 사용자 이름 및 비밀번호 인증을 활성화 하려면 accounts-password을 추가하기 만하면됩니다.<br/>
+> 설치 : meteor add accounts-password , meteor npm install --save bcrypt 
+
+## 클라이언트 측 데이터베이스 권한을 취소 
+
+> 설치 : meteor remove insecure <br/>
+  -> 클라이언트 측에서 데이터베이스에 접근 권한을 차단한다.
